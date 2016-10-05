3f01a24fd248eb63cfd232e1ab3ab0960faeddff
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2016-10-05 15:43:27 -0300

    Finally icon is working

--- app/components/ngMdIcon/ng-md-icon.component.ts
+++ app/components/ngMdIcon/ng-md-icon.component.ts
@@ -1,24 +1,24 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewChild, ElementRef } from "@angular/core";
 import { MdIconService } from "./ng-md-icon.service";
 
 @Component({
-	selector: "icone",
+	selector: "icone",	
 	template: `
-	<svg xmlns="http://www.w3.org/2000/svg" attr.viewBox="{{ViewBox}}" attr.width="{{Size}}" attr.height="{{Size}}" [attr.value]="IconPath">
-		
+	<svg xmlns="http://www.w3.org/2000/svg" [attr.viewBox]="ViewBox" [attr.width]="Size" [attr.height]="Size" [style.fill]="Fill" #iconPath>
 	</svg>`
 })
 export class MdMyIcon {
 	constructor(private iconService: MdIconService) {
 	}
-	
-	get IconPath(): string {
-		var result = this.iconService.Shapes[this.Icon];
-		return result;
-	}
 
-	@Input('icon') Icon : string = "help";
-	@Input("size") Size: number = 24;
+	@ViewChild('iconPath') dataContainer: ElementRef;
+	@Input('icon') set Icon(value: string) {
+		var result = this.iconService.Shapes[value];
+		this.dataContainer.nativeElement.innerHTML = result;	
+	}
+	
+	@Input("size") Size: number = 44;
+	@Input("fill") Fill: string = "red";
 
 	ViewBox: string = "0 0 24 24";
 }
