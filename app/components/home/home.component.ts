2e23b4ae480571edf5106e6369fae8e4d78f31d5
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2016-10-05 19:27:54 -0300

    Working

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -6,5 +6,22 @@ import { Component } from "@angular/core";
     styles: [require("./home.scss")]
 })
 export class HomeComponent {
-    constructor() {}
+    constructor() {
+    }
+
+    public Change(): void {
+        console.log("Run");
+        this.Aux = !this.Aux;
+        this.Icon1 = this.Aux ? "amazon" : "apple";
+        this.Icon2 = !this.Aux ? "amazon" : "apple";
+    }
+
+    public Aux: boolean = true;
+
+    public Icon1: string = "amazon";
+    public Icon2: string = "apple";
+    public Icon3: string = "facebook-box";
+    public Icon4: string = "facebook-messenger";
+    public Icon5: string = "facebook";
+    public Icon6: string = "github-box";
 }
